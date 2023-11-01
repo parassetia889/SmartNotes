d5d2da3bcfad03ec4c7321d5401301bd74c131ce
Paras Setia <parassetia889@gmail.com>
2023-11-01 23:29:28 +0530

    changed urls for class based home views

--- home/urls.py
+++ home/urls.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-urlpatterns = [path("home/", views.home), path("authorized/", views.authorized)]
+urlpatterns = [path("home/", views.HomeView.as_view), path("authorized/", views.AuthorizedView.as_view)]

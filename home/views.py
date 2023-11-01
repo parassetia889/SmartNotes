1c6e2e34e0c736b3c843da9d1dd699e7852dd332
Paras Setia <parassetia889@gmail.com>
2023-11-01 23:28:44 +0530

    added class based views in Home app

--- home/views.py
+++ home/views.py
@@ -2,12 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
-def home(request):
-    return render(request, 'home/welcome.html', {'today' : datetime.today})
+class HomeView(TemplateView):
+    template_name = 'home/welcome.html'
+    extract_context = {'today' : datetime.today}
 
-@login_required(login_url='/admin')
-def authorized(request):
-    return render(request, 'home/authorized.html', {})
-
+class AuthorizedView(LoginRequiredMixin,TemplateView):
+    template_name = 'home/authorized.html'
+    login_url = '/admin'

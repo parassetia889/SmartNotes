af1e09a09824b4ef1e509646c5447b4f5adc84b0
Paras Setia <parassetia889@gmail.com>
2023-11-01 23:27:17 +0530

    Initial commit

---
+++ notes/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Notes(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)

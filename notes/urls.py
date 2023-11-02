4b93a46e4fca8a6a419cc97cca0f84f3525d948b
Paras Setia <parassetia889@gmail.com>
2023-11-02 22:09:43 +0530

    Added base html with styling

--- notes/urls.py
+++ notes/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("notes/", views.NotesListView.as_view()), path("notes/<int:pk>", views.NotesDetailView.as_view())]
+urlpatterns = [
+    path("notes/", views.NotesListView.as_view(), name="notes.list"), 
+    path("notes/<int:pk>", views.NotesDetailView.as_view(), name="notes.detail")
+               ]
